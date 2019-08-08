20b4ccd8da2c5ad20b5e67a617abb1914745aec4
Apple <apple@Apples-MacBook-Pro-2.local>
2019-08-08 16:12:57 -0700

    done but not pretty

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -19,21 +19,30 @@ class ViewController: UIViewController {
     }
 
     @IBAction func oakTreeTapped(_ sender: UIButton) {
-    var alertController = UIAlertController(title: "Oak Tree", message: "You're oaky!", preferredStyle: UIAlertController.Style.alert)
+         var oakTree = ["You're oaky", "You're tree-mendous!", "I wood love to spend time with you!"]
+        
+        var alertController = UIAlertController(title: oakTree.randomElement(), message: "You're strong and supportive, a true classic! Name the last time you met a bad oak tree, yeah that's what I thought, never", preferredStyle: UIAlertController.Style.alert)
     alertController.addAction(UIAlertAction(title: "very cool!", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
-
+       
     }
     
     @IBAction func evergreenTreeTapped(_ sender: UIButton) {
-        var alertController = UIAlertController(title: "Evergreen Tree", message: "You're absolutely tree-mendous!", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "very cool!", style: UIAlertAction.Style.default, handler: nil))
+        var everGreenTree = ["You make me sappy <3", "You're a treet to be around!", "It's treeson to dislike you"]
+        var alertController = UIAlertController(title: everGreenTree.randomElement(), message: "You're festive and mysetrious, what secrets are you hiding in your branches?", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "absolutely radical!", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil) }
     
     
     @IBAction func palmTreeTapped(_ sender: UIButton) {
-        var alertController = UIAlertController(title: "Palm Tree", message: "I hate it when you leaf", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "very cool!", style: UIAlertAction.Style.default, handler: nil))
+        var palmTree = ["I'm naturally drawn to you", "I hate it when you leaf", "You're extreemely cool"]
+        
+        var alertController = UIAlertController(title: palmTree.randomElement(), message: "You're fun and tropical. You're always brigthening people's moods and in shopping malls in the middle of the midwest for some reason", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "heck yeah!", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil) }
+
+
+
+
 }
 
