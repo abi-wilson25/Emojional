7d7af62e9412a2141bf8ba0ddd4a2838ae5beeae
Apple <apple@Apples-MacBook-Pro-2.local>
2019-08-08 15:49:25 -0700

    its okay

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func oakTreeTapped(_ sender: UIButton) {
+    var alertController = UIAlertController(title: "Oak Tree", message: "You're oaky!", preferredStyle: UIAlertController.Style.alert)
+    alertController.addAction(UIAlertAction(title: "very cool!", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
 
+    }
+    
+    @IBAction func evergreenTreeTapped(_ sender: UIButton) {
+        var alertController = UIAlertController(title: "Evergreen Tree", message: "You're absolutely tree-mendous!", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "very cool!", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil) }
+    
+    
+    @IBAction func palmTreeTapped(_ sender: UIButton) {
+        var alertController = UIAlertController(title: "Palm Tree", message: "I hate it when you leaf", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "very cool!", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil) }
 }
 
